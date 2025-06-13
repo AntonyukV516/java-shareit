f1819b11050d34072347c7e51f0cad8e56ac8a35
Viacheslav Antonyuk <antonyukv516@yandex.ru>
2025-06-13 14:02:24 +0300

    Подправил UserUpdateDto, добавил @Email

--- src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,5 +9,6 @@ import lombok.Data;
 public class UserUpdateDto {
     private Long id;
     private String name;
+    @Email
     private String email;
 }
