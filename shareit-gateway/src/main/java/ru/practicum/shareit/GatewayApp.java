d8ba59c346bb854a0a497632ae4aeccdad527885
Viacheslav Antonyuk <antonyukv516@yandex.ru>
2025-07-24 22:52:30 +0300

    Исправлены ошибки тестов добавлены настройки для тестов

--- shareit-gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
+++ shareit-gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
@@ -2,19 +2,18 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import ru.practicum.shareit.gateway.AppConfig;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		JpaRepositoriesAutoConfiguration.class
+		HibernateJpaAutoConfiguration.class
 })
-public class GetewayApp {
-
+@Import(AppConfig.class)
+public class GatewayApp {
 	public static void main(String[] args) {
-		SpringApplication.run(GetewayApp.class, args);
+		SpringApplication.run(GatewayApp.class, args);
 	}
-
 }
