f5a85fc1bd954a495e6a99782fe10721d12735c0
Viacheslav Antonyuk <antonyukv516@yandex.ru>
2025-08-05 14:45:36 +0300

    Стиль

--- gateway/src/main/java/ru/practicum/shareit/gateway/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/booking/BookingController.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.gateway.RestClientUtils;
 import ru.practicum.shareit.gateway.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.gateway.booking.dto.BookingResponseDto;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
